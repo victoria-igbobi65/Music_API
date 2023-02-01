00246b22b91ea6a1c3b09160c01a6bfb9f8a2d5f
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-01 21:22:59 +0100

    updated file

--- src/constants/ts.js
+++ src/constants/ts.js
@@ -2,22 +2,6 @@ module.exports = {
     DEVELOPMENT: 'development',
     PRODUCTION: 'production',
 
-    ACCOUNT_TYPES: {
-        USER: 'user',
-        ADMIN: 'admin',
-    },
-
-    WALLET_TYPE: {
-        BUSINESS: 'business',
-        USER: 'user',
-    },
-
-    ROLE_TYPE: {
-        CONTROL: 'control',
-        SUPPORT: 'support',
-        NONE: 'none',
-    },
-
     DEVELOPMENT: 'development',
     PRODUCTION: 'production',
 
@@ -26,43 +10,12 @@ module.exports = {
         ADMIN: 'admin',
     },
 
-    ROLE_TYPE: {
-        CONTROL: 'control',
-        SUPPORT: 'support',
-        NONE: 'none',
-    },
-    EVENT_TYPE: {
-        ORDER: 'order_event',
-    },
-    PRODUCT_STATUS: {
-        APPROVED: 'approved',
-        PENDING: 'pending',
-        DISSAPPROVED: 'disapproved',
-    },
     ACCOUNT_STATUS: {
         APPROVED: 'approved',
         PENDING: 'pending',
         DISSAPPROVED: 'disapproved',
     },
-    ORDER_STATUS: {
-        SHIPPED: 'shipped',
-        PENDING: 'pending',
-        COMPLETED: 'completed',
-        INCOMPLETED: 'incompleted',
-        CANCELLED: 'cancelled',
-        CONFIRMED: 'confirmed',
-    },
-    TRANSACTION_STATUS: {
-        SUCCESS: 'success',
-        PENDING: 'pending',
-        COMPLETED: 'completed',
-        FAILED: 'failed',
-    },
-    TOKEN_FLAG: {
-        AUTH: 'auth',
-        EMAIL_VERIFY: 'email_verification',
-        RESET: 'password_reset',
-    },
+  
     MESSAGE: {
         ERROR: {
             LOGIN: 'invalid credentials',
@@ -83,7 +36,7 @@ module.exports = {
         EMAIL_VERIFICATION: 'email_verification',
     },
     LINKS: {
-        ORIGIN: 'http://localhost:3032',
+        RESETPASSWORD: 'http://localhost:500/auth/resetPassword/',
     },
     TOKEN: {
         NAME: 'jwt_token',
