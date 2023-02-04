54312dab23a50fee2e73e8b611730326f4dcc374
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-04 23:44:52 +0100

    updated schema

--- src/token/accesstokenschema.js
+++ src/token/accesstokenschema.js
@@ -7,15 +7,15 @@ const spotifySchema = new Schema({
         type: String,
         required: [true, 'Please provide access token']
     },
-    expiry: {
+    expires: {
         type: Date,
-        required: [true, "please provide expiry time"]
+
     }
 })
 
 
 spotifySchema.pre('save', async function(){
-    this.expiry = Date.now() + 3600 * 1000;
+    this.expires = Date.now() + 3600 * 1000;
 })
 
 const spotifyAccessToken = mongoose.model('accesstoken', spotifySchema)
