c4af45c0857e147c4f9902bddeeff626a6d24347
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-05 18:59:31 +0100

    updated schema file

--- src/tracks/trackschema.js
+++ src/tracks/trackschema.js
@@ -7,17 +7,28 @@ const trackSchema = new Schema({
     trackid: {
         type: String,
         required: [true, 'Track ID required!'],
-        unique: true
+        unique: true,
     },
     name: {
         type: String,
-        required: [true, 'Track name required!']
+        required: [true, 'Track name required!'],
     },
     previewurl: {
-        type: String
+        type: String,
     },
-    artistname: String,
-    releasedate: Date
+    artistname: {
+        type: String,
+        required: true
+    },
+    artisttype: {
+        type: String,
+        required: [true, 'Provide artist type'],
+    },
+    releasedate: Date,
+    images: [{
+        type: String,
+    }],
+    albumtype: String
 })
 
 const trackSchema1 = mongoose.model('track', trackSchema)
