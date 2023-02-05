29a1ad30653f218d5ac1ff4686bb594d7ec96834
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-05 18:58:55 +0100

    set up route for track routes

--- src/tracks/trackroute.js
+++ src/tracks/trackroute.js
@@ -1,13 +1,32 @@
 const express = require('express')
 
 const trackController = require('./ts')
-
+const userMiddleware = require('../user/middleware')
+const trackMiddleware = require('./middleware')
 
 const router = express.Router()
 
 router
+    .route('/:id/like')
+    .get(userMiddleware.checkToken, trackMiddleware.validateTrackId, trackController.likeaTrack)
+
+router
+    .route('/:id/share')
+    .get(trackMiddleware.validateTrackId, trackController.shareaTrack)
+
+
+router
+    .route('/:id/unlike')
+    .get(userMiddleware.checkToken, trackMiddleware.validateTrackId, trackController.unlikeaTrack)
+
+
+router
+    .route('/:id/unlike')
+    .get(userMiddleware.checkToken, trackMiddleware.validateTrackId, trackController.dislikeaTrack)
+router
     .route('/:id')
-    .get(trackController.getaTrack)
+    .get(trackMiddleware.validateTrackId, trackController.getaTrack)
+
 
 
 module.exports=router;
