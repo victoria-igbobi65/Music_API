def0b9bf477af4710a2679da1816e68622c17399
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-05 19:00:36 +0100

    imported some helper functions and added some controllers

--- src/tracks/ts.js
+++ src/tracks/ts.js
@@ -1,17 +1,83 @@
 const { StatusCodes } = require('http-status-codes');
+
 const catchAsync = require('../utils/catchAsync')
-const { apiCall, createTrack } = require('./trackservices')
 const CONSTANTS = require('../constants/ts')
+const HELPER = require('../utils/helper')
+
+const { apiCall, createTrack, getTrackId } = require('./trackservices')
+const { createLike, deleteLike } = require('../features/likeservices');
+const { createDislike } = require('../features/dislikeservices')
+
+
 
 exports.getaTrack = catchAsync( async( req, res ) => {
     
     const trackId = req.params.id;
     
-    const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`
+    const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`;
     const track = await apiCall(url);
+
+    const object = HELPER.destructureObject({ ...track })
+    await createTrack(object) 
     
     res.status(StatusCodes.OK).json({
-        track
+        track,
+
+    })
+
+})
+
+
+exports.likeaTrack = catchAsync( async( req, res) => {
+
+    const id = await getTrackId( { trackid: req.params.id} )
+    const userId = req.user;
+
+    await create( { songid: id, userid: userId } );
+    res.status(StatusCodes.OK).json({
+        status: true,
+        msg: null
+    })
+
+})
+
+
+exports.shareaTrack = catchAsync( async( req, res ) => {
+    const trackId = req.params.id;
+
+    const trackUrl = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`;
+    res.status(StatusCodes.OK).json({
+        status: true,
+        msg: trackUrl
+    })
+})
+
+
+exports.unlikeaTrack = catchAsync( async( req, res) => {
+
+    const userId = req.user;
+    const trackId = req.params.id
+
+    const track = await getTrackId({ trackid: trackId})
+    await deleteLike({ songid: track.id, userid: userId })
+
+    res.status( StatusCodes.OK ).json({
+        status: true,
+        msg: null
+    })
+
+})
+
+
+exports.dislikeaTrack = catchAsync( async( req, res ) => {
+
+    const id = await getTrackId({ trackid: req.params.id })
+    const userId = req.user
+
+    await createDislike({ songid: id, userid: userId })
+    res.status(StatusCodes.OK).json({
+        status: true,
+        msg: null,
     })
 
 })
