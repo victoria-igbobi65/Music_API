633685b6b9efb704632457a13e15c403b731a810
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-05 20:19:53 +0100

    updated file

--- src/user/ts.js
+++ src/user/ts.js
@@ -1,4 +1,4 @@
-const STATUSCODES = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 
 const {getUser, getAllUsers, updateUser, deleteUser} = require('../auth/authservices')
 const catchAsync = require('../utils/catchAsync')
@@ -13,7 +13,7 @@ exports.getaUser = catchAsync( async( req, res ) => {
     const user = await getUser({ _id: id });
 
     user.password = undefined;
-    res.status(STATUSCODES.OK).json({
+    res.status( StatusCodes.OK ).json({
         status: true,
         user: user
     })
@@ -23,7 +23,7 @@ exports.getaUser = catchAsync( async( req, res ) => {
 exports.getAllUser = catchAsync( async( req, res) => {
 
     const users = await getAllUsers({ usertype: CONSTANTS.ACCOUNT_TYPES.USER });
-    res.status(STATUSCODES.OK).json({
+    res.status( StatusCodes.OK ).json({
         status:true,
         nhbits: users.length,
         users: users
@@ -35,8 +35,8 @@ exports.deleteaUser = catchAsync( async( req, res) => {
 
     const id = req.params.id
 
-    await deleteUser({_id: id})
-    res.status(STATUSCODES.OK).json({
+    await deleteUser({ _id: id })
+    res.status( StatusCodes.OK ).json({
         status: true,
         msg: "Account deleted succesfully!"
     })
@@ -45,17 +45,17 @@ exports.deleteaUser = catchAsync( async( req, res) => {
 
 exports.updateaUser = catchAsync( async( req, res ) => {
 
-    const {firstname, lastname, email, phonenumber, profilepicurl} = req.body;
+    const { firstname, lastname, email, phonenumber, profilepicurl} = req.body;
     const id = req.params.id;
 
-    const queryObj = HELPER.buildQuery({firstname, lastname, email, phonenumber, profilepicurl})
+    const queryObj = HELPER.buildQuery({ firstname, lastname, email, phonenumber, profilepicurl})
     const updatedUser = await updateUser(
         id,
-        {$set: queryObj},
+        { $set: queryObj },
         { new: true, runValidators: true }
     )
 
-    res.status( STATUSCODES.OK ).json({
+    res.status( StatusCodes.OK ).json({
         status: true,
         msg: 'Update Successful!',
         updatedUser: updatedUser
