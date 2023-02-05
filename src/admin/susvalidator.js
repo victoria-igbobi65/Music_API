836c33e764fd2a87e81283621ceafce4c839e75d
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-05 20:16:46 +0100

    validator file

---
+++ src/admin/susvalidator.js
@@ -0,0 +1,26 @@
+const joi = require('@hapi/joi')
+
+const validateBody = (schema) => {
+    return (req, res, next) => {
+        const result = schema.validate(req.body)
+
+        if (result.error) {
+            return res.status(400).json({ message: result.error.message })
+        }
+
+        next()
+    }
+}
+
+const reasonSchema = joi.object().keys({
+    reason: joi
+        .string()
+        .min(2)
+        .max(255)
+        .error(new Error('Reason for suspension is required!')),
+    
+})
+
+const validatesuspensionBody = validateBody(reasonSchema)
+
+module.exports = { validatesuspensionBody };
