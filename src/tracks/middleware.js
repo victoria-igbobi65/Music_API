eb0cceb161818e30a0af08b0e53532a1cc332d3d
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-06 23:06:31 +0100

    updated file

--- src/tracks/middleware.js
+++ src/tracks/middleware.js
@@ -2,13 +2,14 @@ const { StatusCodes } = require('http-status-codes')
 
 const AppError = require('../utils/appError')
 const { getTrackId } = require('./trackservices')
+const catchAsync = require('../utils/catchAsync')
 
-exports.validateTrackId = async( req, res, next) => {
-    const trackId = req.params.id;
+exports.validateTrackId = catchAsync(async ( req, res, next ) => {
+    const trackId = req.params.trackid;
 
     const found = await getTrackId({ trackid: trackId })
-    if (!found){
-        throw new AppError(`Track with ID: ${trackId} not found`, 400)
+    if (!found) {
+        throw new AppError(`Track with ID: ${trackId} not found`, StatusCodes.BAD_REQUEST )
     }
-    next();
-}
+    next()
+})
