34ce362fca307828e9b32d4f40eabda41e06396f
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-06 23:06:52 +0100

    updated file

--- src/tracks/ts.js
+++ src/tracks/ts.js
@@ -6,16 +6,21 @@ const HELPER = require('../utils/helper')
 
 const { apiCall, createTrack, getTrackId } = require('./trackservices')
 const { createLike, deleteLike } = require('../features/likeservices');
-const { createDislike } = require('../features/dislikeservices')
+const { createDislike } = require('../features/dislikeservices');
+const AppError = require('../utils/appError');
 
 
 
 exports.getaTrack = catchAsync( async( req, res ) => {
     
-    const trackId = req.params.id;
+    const trackId = req.params.trackid;
     
     const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`;
     const track = await apiCall(url);
+
+    if (track.error){
+        throw new AppError(`Invalid track ID: ${trackId}`, track.error.status)
+    }
 
     const object = HELPER.destructureObject({ ...track })
     await createTrack(object) 
@@ -30,10 +35,10 @@ exports.getaTrack = catchAsync( async( req, res ) => {
 
 exports.likeaTrack = catchAsync( async( req, res) => {
 
-    const id = await getTrackId( { trackid: req.params.id} )
+    const id = await getTrackId( { trackid: req.params.trackid} )
     const userId = req.user;
 
-    await create( { songid: id, userid: userId } );
+    await createLike( { songid: id, userid: userId } );
     res.status(StatusCodes.OK).json({
         status: true,
         msg: null
@@ -43,9 +48,9 @@ exports.likeaTrack = catchAsync( async( req, res) => {
 
 
 exports.shareaTrack = catchAsync( async( req, res ) => {
-    const trackId = req.params.id;
+    const trackId = req.params.trackid;
 
-    const trackUrl = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`;
+    const trackUrl = `${CONSTANTS.LINKS.APPBASEURL}tracks/${trackId}`;
     res.status(StatusCodes.OK).json({
         status: true,
         msg: trackUrl
@@ -56,7 +61,7 @@ exports.shareaTrack = catchAsync( async( req, res ) => {
 exports.unlikeaTrack = catchAsync( async( req, res) => {
 
     const userId = req.user;
-    const trackId = req.params.id
+    const trackId = req.params.trackid
 
     const track = await getTrackId({ trackid: trackId})
     await deleteLike({ songid: track.id, userid: userId })
@@ -71,13 +76,49 @@ exports.unlikeaTrack = catchAsync( async( req, res) => {
 
 exports.dislikeaTrack = catchAsync( async( req, res ) => {
 
-    const id = await getTrackId({ trackid: req.params.id })
+    const id = await getTrackId({ trackid: req.params.trackid })
     const userId = req.user
 
     await createDislike({ songid: id, userid: userId })
     res.status(StatusCodes.OK).json({
         status: true,
         msg: null,
+    })
+
+})
+
+exports.getRelatedTracks = catchAsync( async( req, res ) => {
+
+    const trackId = req.params.id;
+    const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}recommendations?seed_tracks=${trackId}&limit=5`
+    const tracks = await apiCall(url)
+
+    if (tracks.error) {
+        throw new AppError(`Invalid track ID: ${trackId}`, tracks.error.status)
+    }
+
+
+    res.status(StatusCodes.OK).json({
+        status: true,
+        nbhits: tracks.tracks.length,
+        relatedtracks: tracks,
+    })
+})
+
+exports.getNewRealease = catchAsync( async( req, res) => {
+
+    const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}browse/new-releases`
+    const tracks = await apiCall( url )
+
+    if (tracks.error) {
+        throw new AppError(`Invalid track ID: ${trackId}`, tracks.error.status)
+    }
+
+
+    res.status( StatusCodes.OK ).json({
+        status: true,
+        nbhits: tracks.albums.items.length,
+        newReleases: tracks
     })
 
 })
