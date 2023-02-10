edbf119ccf34ad2320268690e95139139f727e9f
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-02-10 23:17:12 +0100

    modified middleware

--- src/tracks/middleware.js
+++ src/tracks/middleware.js
@@ -1,15 +1,25 @@
 const { StatusCodes } = require('http-status-codes')
 
 const AppError = require('../utils/appError')
-const { getTrackId } = require('./trackservices')
+const CONSTANTS = require('../constants/ts')
+const HELPER = require('../utils/helper')
+const { getTrackId, apiCall, createTrack } = require('./trackservices')
 const catchAsync = require('../utils/catchAsync')
 
 exports.validateTrackId = catchAsync(async ( req, res, next ) => {
     const trackId = req.params.trackid;
 
-    const found = await getTrackId({ trackid: trackId })
-    if (!found) {
-        throw new AppError(`Track with ID: ${trackId} not found`, StatusCodes.BAD_REQUEST )
+    const song = await getTrackId({ trackid: trackId })
+    if (!song) {
+
+        const url = `${CONSTANTS.LINKS.SPOTIFYREQUESTBASEURL}tracks/${trackId}`
+        const track = await apiCall(url)
+        if (!track) {
+            throw new AppError(`Track with ID: ${trackId} doesn't exist`, StatusCodes.BAD_REQUEST )
+        }
+        
+        const object = HELPER.destructureObject({ ...track })
+        await createTrack(object)
     }
     next()
 })
